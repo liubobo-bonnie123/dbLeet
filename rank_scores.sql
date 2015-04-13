92d643c05a2c1f922458b7c408ad85548a6a7d7d
Bo Liu <emmaliu.2010@gmail.com>
2015-04-12 23:45:36 -0400

    Update rank_scores.sql

--- rank_scores.sql
+++ rank_scores.sql
@@ -1,2 +1,25 @@
-SELECT t.Score, t.Rank FROM (SELECT Score, @prev := @curr, @curr := Score, @rank := IF(@prev = @curr, @rank, @rank + 1) AS Rank
-FROM Scores, (SELECT @curr := null, @prev := null, @rank := 0) sel1 ORDER BY Score DESC) t
+SELECT Scores.Score, COUNT(Ranking.Score) AS RANK
+  FROM Scores
+     , (
+       SELECT DISTINCT Score
+         FROM Scores
+       ) Ranking
+ WHERE Scores.Score <= Ranking.Score
+ GROUP BY Scores.Id, Scores.Score
+ ORDER BY Scores.Score DESC;
+ 
+ 
+ 
+ 
+ 
+SELECT Scores.Score, Q3.Rank
+FROM(
+    SELECT Q1.Score as Score, COUNT(Q1.Score) as Rank
+    FROM 
+        (SELECT DISTINCT Score from Scores) as Q1,
+        (SELECT DISTINCT Score from Scores) as Q2
+    WHERE Q1.Score <= Q2.Score
+    GROUP BY Q1.Score
+    ) as Q3, Scores
+WHERE Q3.Score = Scores.Score
+ORDER BY Scores.Score DESC
