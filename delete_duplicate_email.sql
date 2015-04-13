53c94aaca08056c54ba6554210ada5b8da999107
Bo Liu <emmaliu.2010@gmail.com>
2015-04-13 18:38:27 -0400

    Update delete_duplicate_email.sql

--- delete_duplicate_email.sql
+++ delete_duplicate_email.sql
@@ -1,4 +1,23 @@
-/*https://leetcode.com/discuss/29787/simple-solution*/
+/*https://leetcode.com/discuss/29787/simple-solution
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
 
 DELETE p1
 FROM Person p1, Person p2
