b3b40c55d0857fc53779cadb6b8503b890dd5a28
Bo Liu <emmaliu.2010@gmail.com>
2015-04-12 23:58:56 -0400

    Update consecutive_numbers.sql

--- consecutive_numbers.sql
+++ consecutive_numbers.sql
@@ -1,3 +1,10 @@
-SELECT DISTINCT t.`Num` FROM (
-  SELECT `Num`, @prev := @curr, @curr := `Num`, @count := IF(@prev = @curr, @count + 1, 1) AS Count
-  FROM Logs, (SELECT @curr := null, @prev := null, @count := 1) sel1) t WHERE t.Count >= 3
+select distinct Num from (
+    select
+        Num,
+        case
+            when @prevNum = Num then @count := @count + 1
+            when (@prevNum := Num) is not null then @count := 1
+        end n
+    from Logs, (select @prevNum := NULL) r
+    order by Id
+) a where n >= 3
