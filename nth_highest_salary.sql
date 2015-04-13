df6e71c0e774308dfa933c272ea6b60e5c890ab1
Bo Liu <emmaliu.2010@gmail.com>
2015-04-12 23:37:19 -0400

    Update nth_highest_salary.sql

--- nth_highest_salary.sql
+++ nth_highest_salary.sql
@@ -1,12 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  DECLARE c, s INT;
-  DECLARE re INT DEFAULT NULL;
-  SET s = N - 1;
-  SELECT count(*) INTO c FROM Employee;
-  IF (N <= c) THEN
-    SELECT DISTINCT (Salary) INTO re FROM Employee ORDER BY Salary DESC LIMIT s, 1;
-  END IF;
-
-  RETURN (re);
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+      
+  );
 END
